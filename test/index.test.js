3eeaab0e598637b346a4fff5d494be562ed138be
carlos123asd <carlos-medin@hotmail.com>
2024-08-27 21:12:00 +0200

    Test jess - ocupancypercentage/isOcupied

---
+++ test/index.test.js
@@ -0,0 +1,50 @@
+const {rooms} = require("../utils/index.js")
+const {bookings} = require("../utils/index.js")
+
+
+describe('Func. isOcupied()', () => {
+    test('of undefined', () => {
+        const result = rooms[0].isOccupied()
+        expect(result).toBe('Date')
+    });
+    test('of string', () => {
+        const result = rooms[0].isOccupied('abcd')
+        expect(result).toBe('Date')
+    });
+    test('of int', () => {
+        const result = rooms[0].isOccupied(12)
+        expect(result).toBe('Date')
+    });
+    test('date YYYY-MM-DD no-ocupied', () => {
+        const result = rooms[1].isOccupied(new Date('2024-08-12'))
+        expect(result).toBe(false)
+    });
+    test('date YYYY-MM-DD ocupied', () => {
+        const result = rooms[0].isOccupied(new Date('2024-08-01'))
+        expect(result).toBe(true)
+    });
+    
+});
+
+describe('Func. occupancyPercentage()', () => {
+    test('of undefined', () => {
+        const result = rooms[0].occupancyPercentage()
+        expect(result).toBe('Dates')
+    });
+    test('of a string and empty', () => {
+        const result = rooms[0].occupancyPercentage('abcd','')
+        expect(result).toBe('Dates')
+    });
+    test('of int', () => {
+        const result = rooms[0].occupancyPercentage(12)
+        expect(result).toBe('Dates')
+    });
+    test('date YYYY-MM-DD 0%', () => {
+        const result = rooms[0].occupancyPercentage(new Date('2023-09-01'),new Date('2023-09-15'))
+        expect(result).toBe(0)
+    });
+    test('date YYYY-MM-DD with %', () => {
+        const result = rooms[0].occupancyPercentage(new Date('2024-08-01'),new Date('2024-08-15'))
+        expect(result).toBe(100)
+    });
+});
---
+++ utils/index.js
@@ -0,0 +1,234 @@
+const dataRoom = [
+    {
+        "name": "Sonia Kris",
+        "bookings": [
+            {
+                "name": "Joyce Schneider",
+                "email": "Joyce.Schneider80@yahoo.com",
+                "checkin": "2024-08-01",
+                "checkout": "2024-08-02",
+                "discount": 5
+            },
+            {
+                "name": "Cedric Johnston",
+                "email": "Cedric_Johnston99@yahoo.com",
+                "checkin": "2024-08-02",
+                "checkout": "2024-08-11",
+                "discount": 5
+            }
+        ],
+        "rate": 269.06,
+        "discount": 18
+    },
+    {
+        "name": "Trevor Romaguera",
+        "bookings": [
+            null
+        ],
+        "rate": 292.45,
+        "discount": 15
+    },
+    {
+        "name": "Tricia Dooley",
+        "bookings": [
+            null
+        ],
+        "rate": 294.49,
+        "discount": 7
+    },
+    {
+        "name": "Shari Hilpert",
+        "bookings": [
+            {"name": "Geneva Murazik",
+            "email": "Geneva.Murazik@gmail.com",
+            "checkin": "2024-08-08",
+            "checkout": "2024-08-26",
+            "discount": 5,}
+        ],
+        "rate": 201.87,
+        "discount": 17
+    },
+    {
+        "name": "Angelina Stoltenberg",
+        "bookings": [
+            null
+        ],
+        "rate": 104.27,
+        "discount": 48
+    },
+    {
+        "name": "Cynthia Erdman",
+        "bookings": [
+            null
+        ],
+        "rate": 268.4,
+        "discount": 33
+    }
+]
+const dataBooking = [
+{
+    "name": "Joyce Schneider",
+    "email": "Joyce.Schneider80@yahoo.com",
+    "checkin": "2024-08-01",
+    "checkout": "2024-08-02",
+    "discount": 5,
+    "room": {
+        "name": "Sonia Kris",
+        "rate": 269.06,
+        "discount": 18
+    }
+},
+{
+    "name": "Cedric Johnston",
+    "email": "Cedric_Johnston99@yahoo.com",
+    "checkin": "2024-08-02",
+    "checkout": "2024-08-11",
+    "discount": 5,
+    "room": {
+        "name": "Sonia Kris",
+        "rate": 269.06,
+        "discount": 18
+    }
+},
+{
+    "name": "Geneva Murazik",
+    "email": "Geneva.Murazik@gmail.com",
+    "checkin": "2024-08-08",
+    "checkout": "2024-08-26",
+    "discount": 5,
+    "room": {
+        "name": "Shari Hilpert",
+        "rate": 201.87,
+        "discount": 17
+    }
+}
+]
+
+class Room {
+    constructor(name,bookings,rate,discount) {
+        this.name = name
+        this.bookings = bookings
+        this.rate = rate * 100
+        this.discount = discount
+    }
+
+    isOccupied(date){
+        let ocuppied = false
+        if(date === undefined || typeof(date) === 'string' || typeof(date) === 'number'){
+            return 'Date'
+        }else{
+            if(this.bookings.length > 0 && this.bookings[0] !== null){
+                for (let index = 0; index < this.bookings.length; index++) {
+                    const booking = this.bookings[index];
+                    if(new Date(booking.checkout) > date){
+                        ocuppied = true
+                    }
+                }
+                if(ocuppied === false){
+                    return false
+                }else{
+                    return true
+                }
+            }else{
+                return false
+            }
+        }
+    }
+    occupancyPercentage(startDate,endDate){
+        let bookingsocuped = []
+        if(startDate === undefined || endDate === undefined || startDate === '' || 
+            endDate === '' || typeof(startDate) === 'number' || typeof(endDate) === 'number'){
+            return 'Dates'
+        }else{
+            for (let index = 0; index < this.bookings.length; index++) {
+                const booking = this.bookings[index]
+                if((new Date(booking.checkin) < startDate && new Date(booking.checkout) > startDate) ||
+                (new Date(booking.checkin) < endDate && new Date(booking.checkout) > endDate) || 
+                (new Date(booking.checkin) < startDate && new Date(booking.checkout) > endDate) ||
+                (new Date(booking.checkout) >= startDate && new Date(booking.checkout) <= endDate)){
+                        bookingsocuped.push(booking)
+                }
+            }
+            if(bookingsocuped.length > 0){
+                //De todos los bookings del Room cuantos estan ocupados dentro del rango 
+                return ((100 * bookingsocuped.length)/this.bookings.length)
+            }else{
+                return 0
+            }
+        }
+    }
+
+    static totalOccupancyPercentage(rooms,startDate,endDate){
+        let roomsocuped = 0;
+        for (let index = 0; index < rooms.length; index++) {
+            const room = rooms[index].bookings;
+            for (let index = 0; index < room.length; index++) {
+                const booking = room[index];
+                if((booking.checkIn <= startDate || booking.checkIn >= startDate || 
+                    booking.checkIn <= endDate) && (booking.checkOut >= startDate ||
+                    booking.checkOut <= endDate || booking.checkOut >= endDate)){
+                        roomsocuped++
+                }
+            }
+        }
+        return ((100 * roomsocuped)/rooms.length)
+    }
+    static availableRooms(rooms,startDate,endDate){
+        let roomsavailables = []
+        let available = true
+        for (let index = 0; index < rooms.length; index++) {
+            const room = rooms[index];
+            for (let index = 0; index < room.bookings.length; index++) {
+                const booking = room.bookings[index];
+                if((booking.checkIn <= startDate || booking.checkIn >= startDate || 
+                    booking.checkIn <= endDate) && (booking.checkOut >= startDate ||
+                    booking.checkOut <= endDate || booking.checkOut >= endDate)){
+                        available = false
+                }
+            }
+            if(available === true){
+                roomsavailables.push(room)
+            }
+        }
+        return roomsavailables
+    }
+}
+
+class Booking {
+    constructor(name,email,checkIn,checkOut,discount,room) {
+        this.name = name
+        this.email = email
+        this.checkIn = checkIn
+        this.checkOut = checkOut
+        this.discount = discount
+        this.room = room
+    }
+
+    getFee(){
+       const price = this.room.rate
+       const discountroom = this.room.discount
+       const discountbooking = this.discount
+       const discountTotal = discountroom + discountbooking
+       return (discountTotal * price) / 100
+    }
+}
+
+const rooms = [
+
+]
+const bookings = [
+
+]
+dataRoom.forEach((room) => {
+    rooms.push(new Room(room.name,room.bookings,room.rate,room.discount))
+})
+dataBooking.forEach((booking) => {
+    bookings.push(new Booking(booking.name,booking.email,booking.checkin,booking.checkout,booking.discount,booking.room))
+})
+
+
+module.exports  = {
+    rooms,
+    bookings
+}
+
